20b006b56798f090b1491cfa29e0eb93b246e49d
nabazesmail <nabazesmail567@gmail.com>
2023-05-17 23:45:29 +0300

    changed the multerconfig

--- config/multerConfig.js
+++ config/multerConfig.js
@@ -1,20 +1,21 @@
-
 const multer = require('multer');
 const path = require('path');
 
 // Setting storage engine for multer
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, path.join(__dirname, '../public/img'));
+    // Specify the directory where you want to store the images
+    cb(null, path.join(__dirname,'../public/img'));
   },
   filename: function (req, file, cb) {
-    cb(null, Date.now() + '-' + file.originalname);
+    // Generate a unique filename for the uploaded image
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    const extension = path.extname(file.originalname);
+    cb(null, 'image-' + uniqueSuffix + extension);
   }
 });
 
 // Initializing multer with the storage engine
-const upload = multer({
-  storage
-});
+const upload = multer({ storage });
 
 module.exports = upload;
